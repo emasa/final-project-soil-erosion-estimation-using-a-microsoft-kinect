7a9f19d0fa3a76e53f8d98a8d16bb036dbdbebf1
emanuel sanchez aimar <emalomas@gmail.com>
2013-09-11 22:17:58 -0300

    registrado el borrado de un viejo archivo

--- source/OpenCVFeaturesFinder.cpp
+++
@@ -1,8 +0,0 @@
-
-#include <pcl/point_types.h>
-#include <pcl/impl/instantiate.hpp>
-
-#include "OpenCVFeaturesFinder.h"
-#include "OpenCVFeaturesFinder.hpp"
-
-PCL_INSTANTIATE_PRODUCT(OpenCVFeaturesFinder, ((pcl::PointXYZRGBA)) ((pcl::PointWithScale)))
