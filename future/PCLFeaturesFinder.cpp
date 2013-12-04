66b75fa2dca9fb2cac8753e1ef5550d383f4b533
emasa <emalomas@gmail.com>
2013-12-04 18:05:18 -0300

    actualizando el estado de la tesis

--- future/PCLFeaturesFinder.cpp
+++
@@ -1,9 +0,0 @@
-
-
-#include <pcl/point_types.h>
-#include <pcl/impl/instantiate.hpp>
-
-#include "PCLFeaturesFinder.h"
-#include "PCLFeaturesFinder.hpp"
-
-PCL_INSTANTIATE_PRODUCT(FPFHEstimationFeaturesFinder, ((pcl::PointXYZRGBA))((pcl::PointWithScale)))
